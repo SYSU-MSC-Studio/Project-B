ba6769681d64302c3b8ec7009766e2bbe1a3a500
Oct <43613676+idontwhatismyname@users.noreply.github.com>
2019-05-17 00:20:22 +0800

    Create CalculateExpression.h

---
+++ CalculateExpression.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#define BigInteger long long
+
+class CalculateExpression{
+    public:
+        explicit CalculateExpression(std::string exper);
+        CalculateExpression(const CalculateExpression &other);
+        CalculateExpression operator=(const CalculateExpression&); // unsure if it needs reference
+        BigInteger calculate();
+        ~CalculateExpression() = default;
+        BigInteger _Result;
+    private:
+        BigInteger CalculateAddSub();
+        BigInteger CalculateMulDivMod();
+        BigInteger CalculateDigit();
+        void clearspace();
+        std::string _Expression;
+        std::string::iterator _Ptr;
+};
